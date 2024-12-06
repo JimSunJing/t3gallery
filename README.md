5267b0be733a3aff51f28c943283c1267cac6b27
JimS <jimsun6428@gmail.com>
2024-12-06 17:48:21 +0800

    basic ui+Top Nav layout

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## TODO LIST
 
-- [ ] 首先将项目部署上（Vercel）
-- [ ] 用临时数据写出网页ui
+- [x] 首先将项目部署上（Vercel）
+- [x] 用临时数据写出网页ui
 - [ ] 设置数据库（Vercel Postgres）
 - [ ] 将数据库数据推送前端
 - [ ] 用户认证（Clerk）
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/A3OWyvtOmv8naoIkqbTSd09syufPh3V5l6KznpNFkD7E24C8",
+  "https://utfs.io/f/A3OWyvtOmv8nAOM94ZtOmv8nw3jxFY0i4PSNrVytgQB9KURD",
+  "https://utfs.io/f/A3OWyvtOmv8nLR4vhfkT60GEYe7P4xUFXDsnbRJgVrAvkNhf",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        Hello (Gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="mock image" />
+          </div>
+        ))}
       </div>
+      Hello (Gallery in progress)
     </main>
   );
 }
