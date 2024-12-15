094f2cb3ad699b08fd81c983532918313b03caac
JimS <jimsun6428@gmail.com>
2024-12-15 14:54:24 +0800

    use grid

--- src/components/full-page-image-view.tsx
+++ src/components/full-page-image-view.tsx
@@ -7,7 +7,7 @@ export default async function FullPageImageView(props: { id: number }) {
       <div className="flex flex-shrink items-center justify-center">
         <img src={image.url} alt={image.name} className="object-contain" />
       </div>
-      <div className="flex w-48 flex-shrink-0 flex-col border-l border-white">
+      <div className="flex w-96 flex-shrink-0 flex-col border-l border-white">
         <div className="text-xl font-bold">{image.name}</div>
       </div>
     </div>
