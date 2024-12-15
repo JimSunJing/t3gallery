be8b51ac695232b9720dba5bca9fc2b53f059e9e
JimS <jimsun6428@gmail.com>
2024-12-15 14:47:01 +0800

    add modal info and style

--- src/components/full-page-image-view.tsx
+++ src/components/full-page-image-view.tsx
@@ -2,5 +2,14 @@ import { getImageById } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImageById(props.id);
-  return <img src={image.url} alt={image.name} className="w-96" />;
+  return (
+    <div className="flex h-full w-full">
+      <div className="flex flex-shrink items-center justify-center">
+        <img src={image.url} alt={image.name} className="object-contain" />
+      </div>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l border-white">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }
